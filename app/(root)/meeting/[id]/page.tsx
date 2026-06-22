91fcd29df2c5b50a49fee5fddae50d4c3aec92d0
anagha2312 <anagha.prajapati@students.iiit.ac.in>
2026-06-22 18:53:02 +0530

    made some changes

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import { useParams } from 'next/navigation';
-import { Loader } from 'lucide-react';
+import Loader from '@/components/Loader';
 
 import { useGetCallById } from '@/hooks/useGetCallById';
 import Alert from '@/components/Alert';
